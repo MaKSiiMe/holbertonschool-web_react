4631400f80a76197fff073e9d4f0cedc0f01ed09
MaKSiiMe <maxime.truel@gmail.com>
2025-05-26 16:16:17 +0200

    Suppression de la liste d'étudiants et du code de génération de tableau dans main.ts

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -18,24 +18,3 @@ const student2: Student = {
   age: 22,
   location: "Lyon"
 };
-
-const studentsList: Student[] = [student1, student2];
-
-const table = document.createElement('table');
-const tbody = document.createElement('tbody');
-
-studentsList.forEach((student) => {
-  const row = document.createElement('tr');
-  const firstNameCell = document.createElement('td');
-  const locationCell = document.createElement('td');
-
-  firstNameCell.textContent = student.firstName;
-  locationCell.textContent = student.location;
-
-  row.appendChild(firstNameCell);
-  row.appendChild(locationCell);
-  tbody.appendChild(row);
-});
-
-table.appendChild(tbody);
-document.body.appendChild(table);
